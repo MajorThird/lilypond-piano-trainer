d930341f54cd1bff216a4b9f2792079c630f57c8
MajorThird <test@testtesttest.org>
2020-05-04 21:39:43 +0200

    load staffs

--- creator.py
+++ creator.py
@@ -1,14 +1,20 @@
 import create_options
-
+import os
+import shutil
+import staff
 
 def delete_and_create_folder(f):
     if os.path.isdir(f):
         shutil.rmtree(f)
-    os.mkdir(f)
+    os.makedirs(f) # works recursively
 
 def main():
     options = create_options.get_options()
     delete_and_create_folder(options["output_folder_complete"])
+    staffs = staff.get_staffs(options["lily_file"])
+
+
+
 
 if __name__ == '__main__':
     main()
