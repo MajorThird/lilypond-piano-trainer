50f0b020821852b4822d71898c76bb0b758982cb
MajorThird <test@testtesttest.org>
2020-05-04 22:22:03 +0200

    move stuff

--- creator.py
+++ creator.py
@@ -1,18 +1,12 @@
 import create_options
-import os
-import shutil
 import staff
-
-def delete_and_create_folder(f):
-    if os.path.isdir(f):
-        shutil.rmtree(f)
-    os.makedirs(f) # works recursively
+import misc_helpers
 
 def main():
     options = create_options.get_options()
-    delete_and_create_folder(options["output_folder_complete"])
+    misc_helpers.delete_and_create_folder(options["output_folder_complete"])
     staffs = staff.get_staffs(options["lily_file"])
-
+    misc_helpers.write_debug_file(staffs, options["output_folder_complete"])
 
 
 
