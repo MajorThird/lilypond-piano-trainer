fb211fbeba744b4d864132373b646bfab0fd05e0
MajorThird <test@testtesttest.org>
2020-05-04 21:17:04 +0200

    create folder

--- creator.py
+++ creator.py
@@ -1,7 +1,14 @@
 import create_options
 
+
+def delete_and_create_folder(f):
+    if os.path.isdir(f):
+        shutil.rmtree(f)
+    os.mkdir(f)
+
 def main():
     options = create_options.get_options()
+    delete_and_create_folder(options["output_folder_complete"])
 
 if __name__ == '__main__':
     main()
