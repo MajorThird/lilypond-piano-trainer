d010c6635ff3a5b91b10e6bb3398b4aedd26697e
MajorThird <test@testtesttest.org>
2020-05-04 22:24:29 +0200

    Make it read chords.

--- creator.py
+++ creator.py
@@ -1,13 +1,14 @@
 import create_options
 import staff
 import misc_helpers
+import chords
 
 def main():
     options = create_options.get_options()
     misc_helpers.delete_and_create_folder(options["output_folder_complete"])
     staffs = staff.get_staffs(options["lily_file"])
     misc_helpers.write_debug_file(staffs, options["output_folder_complete"])
-
+    all_chord_symbols = chords.get_chords(filename)
 
 
 if __name__ == '__main__':
